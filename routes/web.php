d7b40ffbf8e3bea876fce3b40b284b96f0b86fb0
AKV85 <a.kotov.laknojus@gmail.com>
2026-09-06 01:33:21 +0300

    SD-33 Redirect root route to application

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,13 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TicketAiController;
 use App\Http\Controllers\TicketAttachmentController;
 use App\Http\Controllers\TicketController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Auth::check()
+        ? redirect()->route('dashboard')
+        : redirect()->route('login');
 });
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
